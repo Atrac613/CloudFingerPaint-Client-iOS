0b2d9ed53bc7add3777a2b700bd848c21666b3dc
Osamu Noguchi <atrac613@gmail.com>
2011-03-01 00:53:34 +0900

    Rename some resource files.

---
+++ Classes/CloudFingerPaintAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  CloudFingerPaint_Client_iOSAppDelegate.h
+//  CloudFingerPaint-Client-iOS
+//
+//  Created by Noguchi Osamu on 11/03/01.
+//  Copyright 2011 envision. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CloudFingerPaintAppDelegate : NSObject <UIApplicationDelegate> {
+    
+    UIWindow *window;
+    UINavigationController *navigationController;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
+
